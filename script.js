6b56847dc88b782e4101c30606bf83a64d9a770f
EkaterinaMash <ekaterina.mashinskayaa@gmail.com>
2022-11-12 15:04:01 +0300

    feat: add elements to the page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-fetch('books-info.json') 
+fetch('https://raw.githubusercontent.com/EkaterinaMash/book-shop/gh-pages/books-info.json') 
         .then(response => {
             return response.json();
         })
@@ -17,25 +17,28 @@ fetch('books-info.json')
             let author = document.createElement('div');
             let bookName = document.createElement('div');
             let bookPrice = document.createElement('div');
+            let bookDescription = document.createElement('button');
 
             book.classList.add('book');
             bookCapture.classList.add('book-capture');
             author.classList.add('author');
             bookName.classList.add('book-name');
             bookPrice.classList.add('book-price');
+            bookDescription.classList.add('book-description');
 
             author.textContent = obj.author;
             bookName.textContent = obj.title;
             bookPrice.textContent = obj.price;
+            bookDescription.textContent = 'Show more';
             bookPic.setAttribute('src', obj.imageLink);
         
-
             let page = document.querySelector('main');
             
             book.appendChild(bookPic);
             bookCapture.appendChild(author);
             bookCapture.appendChild(bookName);
             bookCapture.appendChild(bookPrice);
+            bookCapture.appendChild(bookDescription);
             book.appendChild(bookCapture);
             page.appendChild(book);
         }
